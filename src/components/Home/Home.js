7b30e005d05e159f912b7a04e4a4101c532188ae
shariyer <s.m.shariyer04@gmail.com>
2022-10-11 03:35:17 +0600

    home content added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css'
 
 const Home = () => {
     return (
         <div className='home-container'>
-            <img  src="quiz-cover.jpg" alt="" />
+            <img src="quiz-cover.jpg" alt="" />
+            
+            <div className='home-texts'>
+                <h1 className='home-text-header'>Welcome To Quizer'S Pro</h1>
+                <p className='home-text-details'>Shine your knowledge by participating on online Quiz. Select your Quiz <Link to='/topic'>Topic</Link>  from menu-bar and prepare your self for bigger challenges. Happy Quizes!! Good Luck!!</p>
+            </div>
+
+           
             
         </div>
     );
