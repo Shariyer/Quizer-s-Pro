3020ac0747e6ec5f3ef20e919fb445be5c876563
shariyer <s.m.shariyer04@gmail.com>
2022-10-11 03:04:20 +0600

    header added and header css added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
     return (
-        <nav>
-            <div>
-                <h2>this is header</h2>
+        <nav className='nav-container'>
+            <div className='nav-header-name'>
+                <h2>Quizer'S Pro</h2>
+            </div>
+            <div className='nav-links'>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/topic'}>Topic</Link>
+                <Link to={'/statistics'}>Statistics</Link>
+                <Link to={'/blog'}>Blog</Link>
             </div>
         </nav>
     );
