ae51d4b0749eff9cb4b3ac90fcd24c88d71a62d6
shariyer <s.m.shariyer04@gmail.com>
2022-10-11 02:01:15 +0600

    base components and layout added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav>
+            <div>
+                <h2>this is header</h2>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/components/Topic/Topic.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Topic = () => {
+    return (
+        <div>
+            <h4>this is Topic</h4>
+        </div>
+    );
+};
+
+export default Topic;
