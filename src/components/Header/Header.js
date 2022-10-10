0da146b7d2c10212a60ff812bb4fa80d440cbafd
shariyer <s.m.shariyer04@gmail.com>
2022-10-11 03:55:56 +0600

    icon and tittle added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'
+import { PuzzlePieceIcon } from '@heroicons/react/24/solid'
+
 
 const Header = () => {
     return (
         <nav className='nav-container'>
             <div className='nav-header-name'>
-                <h2>Quizer'S Pro</h2>
+                <h2>Quizer'S Pro<PuzzlePieceIcon className="icon"/></h2>
+                
             </div>
             <div className='nav-links'>
                 <Link to={'/'}>Home</Link>
