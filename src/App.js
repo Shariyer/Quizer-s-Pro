765bfdfe9ba0bd360a6d3780e2e1f87a8cfe914f
shariyer <s.m.shariyer04@gmail.com>
2022-10-12 16:04:40 +0600

    error page added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Statistics from './components/Statistics/Statistics';
 import Quizes from './components/Quizes/Quizes';
 import Main from './layouts/Main';
 import Questions from './components/Questions/Questions';
+import Error from './components/Error/Error';
 
 function App() {
 
@@ -13,25 +14,30 @@ function App() {
     {
       path: '/',
       element: <Main></Main>,
+      errorElement:<Error></Error>,
       children: [
         {
           path: '/',
-          element:<Home></Home>
+          element: <Home></Home>,
+          errorElement:<Error></Error>
         },
         {
           path: '/quizes',
           loader: async ()=>fetch('https://openapi.programming-hero.com/api/quiz'),
           element: <Quizes></Quizes>,
+          errorElement:<Error></Error>
           
         },
         {
           path: '/statistics',
           loader: async ()=>fetch('https://openapi.programming-hero.com/api/quiz'),
-          element:<Statistics></Statistics>
+          element: <Statistics></Statistics>,
+          errorElement:<Error></Error>
         },
         {
           path: '/blog',
-          element:<Blog></Blog>
+          element: <Blog></Blog>,
+          errorElement:<Error></Error>
         },
         {
           path: '/quizes/:questionId',
@@ -39,7 +45,8 @@ function App() {
           {
             return fetch(`https://openapi.programming-hero.com/api/quiz/${params.questionId}`)
           },
-          element:<Questions></Questions>,
+          element: <Questions></Questions>,
+          errorElement:<Error></Error>
         }
       ]
     }
--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
+import './Error.css'
 
 const Error = () => {
+    const error = useRouteError();
     return (
-        <div>
-            
+        <div className='Error-container'>
+            <h2 className='error-header'>Opps!!!! an Error Ocurred</h2>
+            <br />
+            {error && (
+                <div>
+                    <h3 className='error-details'>{error.status}</h3>
+                    <h3 className='error-details'>{error.statusText || error.message}</h3>
+                </div>
+            )}
+
         </div>
     );
 };
